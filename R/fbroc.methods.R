7ed260c8ae4aa47d7148a8fc613bec1a92c7890e
Erik Peter <jerikpeter@googlemail.com>
2015-04-29 00:00:00 +0000

    version 0.1.0

---
+++ R/fbroc.methods.R
@@ -0,0 +1,171 @@
+#' Prints information about a \code{fbroc.perf} object
+#' 
+#' Prints the information about the bootstrap results for an object of class
+#' \code{fbroc.perf}. This information includes the number of bootstrap
+#' replicates, the metric used and the estimate with confidence interval.
+#' @param x Object of class \code{fbroc.perf}.
+#' @param ... further arguments passed to or from other methods.
+#' @return Character containing the text that is also printed.
+#' @seealso \code{\link{perf.roc}}
+#' @export
+print.fbroc.perf <- function(x, ...) {
+  conf.level <- round(100 * x$conf.level, 0)
+  text <- paste("\n", "
+                Bootstrapped ROC performance metric", "\n", "\n",
+                "Metric: ", x$metric, "\n",
+                "Bootstrap replicates: ", x$n.boot, "\n", 
+                "Estimate: ", round(x$Observed.Performance, 2), "\n",
+                "Std. Error: ", round(sd(x$boot.results), 3), "\n", 
+                conf.level,"% confidence interval:", "\n",
+                round(x$CI.Performance[1], 2)," ",
+                round(x$CI.Performance[2], 2), "\n", "\n",
+                sep = "")
+  cat(text)
+  invisible(text)
+}
+
+#' Prints information about a \code{fbroc.roc} object
+#' 
+#' Prints the information about the bootstrap results for an object of class
+#' \code{fbroc.roc}. This information includes the number of bootstrap
+#' replicates, the time spent on bootstrapping, the AUC and the memory
+#' usage of the object.
+#' @param x Object of class \code{fbroc.roc}.
+#' @param ... further arguments passed to or from other methods.
+#' @return Character containing the text that is also printed.
+#' @seealso \code{\link{boot.roc}}
+#' @export
+print.fbroc.roc <- function(x, ...) {
+  x.mem <- round(as.numeric(object.size(x))/(1024*1024),0)
+  text <- cat(paste("\n",
+              "Bootstraped ROC Curve with ", x$n.pos, " positive and ", x$n.neg,
+            " negative samples. \n \n", "The AUC is ", round(x$auc, 2),".\n \n", 
+            x$n.boot, " bootstrap samples have been calculated. \n", "The calculation took ", x$time.used, 
+            " seconds and the results use up ", x.mem, " MB of memory.", 
+            "\n", "\n", sep = ""))
+  cat(text)
+  invisible(text)
+}
+
+#' Plots a \code{fbroc.roc} object
+#' 
+#' Plot a \code{fbroc.roc} object and shows the ROC curve. The confidence
+#' region for the ROC curve and the result for a specified performance metric 
+#' can also be included in the plot. 
+#' @param x Object of class \code{fbroc.roc}.
+#' @param col Color used for the curve. Defaults to blue.
+#' @param fill Color used for the confidence region. Defaults to royalblue1.
+#' @param print.plot Logical specifying whether the plot should be printed.
+#' @param show.conf Logical specifying whether the confidence region should be
+#' plotted.
+#' @param conf.level Confidence level of the confidence region.
+#' @param show.metric Character specifying which metric to display. See 
+#' \code{\link{perf.roc}} for details. Defaults to \code{NULL}, which means
+#' that no metric is displayed.
+#' @param ... further arguments passed to or from other methods.
+#' @return A ggplot, so that the user can customize the plot further.
+#' @examples
+#' y <- rep(c(TRUE, FALSE), each = 500)
+#' x <- rnorm(1000) + y
+#' result.boot <- boot.roc(x, y, n.boot = 100)
+#' plot(result.boot)
+#' @seealso \code{\link{boot.roc}}, \code{\link{perf.roc}}
+#' @export
+plot.fbroc.roc <- function(x, col = "blue", fill = "royalblue1", print.plot = TRUE,
+                           show.conf = TRUE, conf.level = 0.95, 
+                           show.metric = NULL, ...) {
+  plot.frame = x$tpr.fpr
+  
+  roc.plot <- ggplot(data = plot.frame, aes(x = FPR, y = TPR)) +               
+    ggtitle("ROC Curve") + xlab("False Positive Rate") +
+    ylab("True Positive Rate") + theme_bw() +
+    theme(title = element_text(size = 22),
+          axis.title.x = element_text(size = 18),
+          axis.title.y = element_text(size = 18),
+          axis.text.x = element_text(size = 16),
+          axis.text.y = element_text(size = 16))
+  
+  if (show.conf) {
+    conf.frame <- conf.roc(x, conf.level = conf.level, steps = 2000)
+    roc.plot <- roc.plot + 
+      geom_ribbon(data = conf.frame, fill = fill, alpha = 0.5,
+                  aes(y = NULL, ymin = Lower.TPR, ymax = Upper.TPR))
+  }
+  if (!is.null(show.metric)) {
+    perf <- perf.roc(x, metric = show.metric, conf.level = conf.level)
+    perf.text <- paste("AUC = ", round(perf$Observed.Performance, 2)," [",
+                       round(perf$CI.Performance[1], 2), ",",
+                       round(perf$CI.Performance[2], 2), "]", sep = "")
+    if (show.metric == "auc") {
+      text.frame <- data.frame(text.c = perf.text, TPR = 0.5, FPR = 0.68)
+      roc.plot <- roc.plot + geom_text(size = 8, aes(label = text.c), data = text.frame)
+    }
+  }
+  roc.plot <- roc.plot + geom_path(size = 1.1, col = col)
+  if (print.plot) print(roc.plot)
+  invisible(roc.plot)
+}
+
+#' Plots ROC based performance metric as histogram
+#' 
+#' Given an object of class \code{perf.roc} this function plots the results of
+#' the bootstrap as an histogram. The confidence interval is also included by
+#' default.
+#' 
+#' @param x Object of class \code{perf.roc} to be plotted.
+#' @param bins Number of bins for histogram. Defaults to between 20 and 60
+#' depending on number of bootstrap replicates.
+#' @param col Color of outline of histogram bars. Defaults to white.
+#' @param fill Fill of histogram bars. Defaults to lightblue.
+#' @param print.plot Logical specifying whether the plot should be printed.
+#' @param show.conf Logical specifying whether the confidence interval
+#' should be displayed.
+#' @param conf.text Logical specifying whether the confidence interval limits
+#' should also be displayed as text.
+#' @param ... Further arguments passed to or from other methods.
+#' @return A ggplot, so that the user can customize the plot further.
+#' @seealso \code{\link{perf.roc}}
+#' @examples
+#' y <- rep(c(TRUE, FALSE), each = 500)
+#' x <- rnorm(1000) + y
+#' result.boot <- boot.roc(x, y, n.boot = 10000)
+#' result.perf <- perf.roc(result.boot, "auc")
+#' plot(result.perf)
+#' @export
+plot.fbroc.perf <- function(x, bins = NULL, col = "white", 
+                            fill = "lightblue", print.plot = TRUE, 
+                            show.conf = TRUE, conf.text = TRUE, ...) {
+  boot.frame <- data.frame(x$boot.results)
+  names(boot.frame) <- "Metric"
+  if (is.null(bins)) {
+    bins <- floor(x$n.boot/200)
+    bins <- max(bins, 20)
+    bins <- min(bins, 60)
+  }
+  bw = round(diff(range(x$boot.results))/bins, 6)
+  perf.plot <- ggplot(data = boot.frame, aes(x = Metric)) + 
+               xlab(toupper(x$metric)) + ylab("Density") + 
+               ggtitle("Performance histogram") +
+               geom_histogram(fill = fill, col = col, aes(, y = ..density..), 
+                              binwidth = bw) + theme_bw() +
+               theme(title = element_text(size = 22),
+                     axis.title.x = element_text(size = 18),
+                     axis.title.y = element_text(size = 18),
+                     axis.text.x = element_text(size = 16),
+                     axis.text.y = element_text(size = 16))
+  if (show.conf) {
+    conf.frame <- data.frame(Metric = x$CI.Performance, y.dummy = 0)
+    perf.plot <- perf.plot + geom_line(data = conf.frame, aes(y=y.dummy), 
+                                       col = "black",
+                                       size = 2)
+    if (conf.text) {
+      conf.frame$text.c <- round(conf.frame$Metric,2)
+      perf.plot <- perf.plot + 
+                     geom_text(data = conf.frame,
+                               aes(x = Metric, y=y.dummy, label = text.c), 
+                               vjust = 0, hjust = c(1,0), size = 8)
+    }
+  }
+  if (print.plot) print(perf.plot)
+  invisible(perf.plot)
+}
---
+++ R/fbroc.roc.R
@@ -0,0 +1,192 @@
+#' Bootstrap ROC curve
+#'
+#' \code{boot.roc} calculates the ROC curve, initializes the settings
+#' and calculates the bootstrap results for the true and false
+#' positive rate at every relevant threshold. Missing values are removed with 
+#' a warning prior to bootstrapping.
+#'
+#' @param pred A numeric vector. Contains predictions. \code{boot.roc} assumes
+#'   that a high prediction is evidence for the observation belonging to the
+#'   positive class.
+#' @param true.class A logical vector. TRUE indicates the sample belonging to the
+#'   positive class.
+#' @param stratify Logical. Indicates whether we use stratified bootstrap.
+#'   Default to TRUE. Non-stratified bootstrap is not yet implemented.
+#' @param n.boot A number that will be coerced to integer. Specified the 
+#'   number of bootstrap replicates. Defaults to 1000.
+#' @return A list of class \code{fbroc.roc}, containing the elements:
+#' \item{prediction}{Input predictions.}
+#' \item{true.class}{Input classes.}
+#' \item{thresholds}{Thresholds.}
+#' \item{n.thresholds}{Number of thresholds.}
+#' \item{n.boot}{Number of bootstrap replicates.}
+#' \item{n.pos}{Number of positive observations.}
+#' \item{n.neg}{Number of negative observations.}
+#' \item{tpr.fpr}{Vector containing true and false positive rates at
+#'                      the different thresholds for the original predictions.}
+#' \item{tpr.fpr.raw}{Vector containing raw results from C++ for later usage by
+#'  other functions.}       
+#' \item{time.used}{Time in seconds used for the bootstrap. Other steps are not
+#' included.}
+#' \item{auc}{The AUC of the original ROC curve.}
+#' \item{tpr.fpr.boot.matrix}{Matrix containing TPR and FPR values at the
+#' thresholds for each bootstrap replicate.}
+#' @examples
+#' y <- rep(c(TRUE, FALSE), each = 500)
+#' x <- rnorm(1000) + y
+#' result.boot <- boot.roc(x, y)
+#' @seealso \code{\link{plot.fbroc.roc}}, \code{\link{print.fbroc.roc}}
+#' @export
+boot.roc <- function(pred, true.class, stratify = TRUE, n.boot = 1000) {
+  # validate input
+  if ((length(pred) != length(true.class)))
+    stop("Predictions and true classes need to have the same length")
+  if ((class(pred) != "numeric"))
+    stop("Predictions must be numeric")
+  if ((class(true.class) != "logical"))
+    stop("Classes must be logical")
+  if ((class(stratify) != "logical"))
+    stop("Classes must be logical")
+  
+  index.na <- is.na(pred) | is.na(true.class)
+  if (any(index.na)) {
+    n <- sum(index.na)
+    warning.msg <- 
+      paste(n, "observations had to be removed due to missing values")
+    warning(warning.msg)
+    true.class <- true.class[!index.na]
+    pred <- pred[!index.na]
+  }
+  
+  if (sum(true.class) == 0)
+    stop("No positive observations are included")
+  if (sum(!true.class) == 0)
+    stop("No negative observations are included")
+  
+  n.boot <- as.integer(n.boot)
+  
+  if (length(n.boot) != 1)
+    stop("n.boot must have length 1")
+  
+  if (length(stratify) != 1)
+    stop("stratify must have length 1")
+  
+  if (!stratify) stop("Non-stratified bootstrapping is not yet supported")
+  
+  thresholds <- calculate.thresholds(pred, true.class)
+  n.thresholds <- length(thresholds)  
+  # Let C++ do the actual work
+  bench <- system.time(tpr.fpr.boot <- 
+                         tpr_fpr_boot(pred, as.integer(true.class),
+                                      thresholds, n.boot))[1]
+  column.names <- paste(rep(c("TPR.AT.T", "FPR.AT.T"), each = n.thresholds),
+                        rep(1:n.thresholds, 2), sep = "")
+  colnames(tpr.fpr.boot) <- column.names
+  
+  bench <- round(bench, 1)
+  tpr.fpr <- true_tpr_fpr(pred, as.integer(true.class), thresholds)
+  tpr.fpr.raw <- tpr.fpr
+  auc <- get_auc(tpr.fpr)
+  tpr.fpr <- data.frame(TPR = tpr.fpr[1:n.thresholds],
+                        FPR = tpr.fpr[(n.thresholds + 1):(2 * n.thresholds)])
+  
+  
+  # prepare output
+  output <- list(predictions = pred,
+                 true.classes = true.class,
+                 thresholds = thresholds,
+                 n.thresholds = n.thresholds,
+                 n.boot = n.boot,
+                 n.pos = sum(true.class),
+                 n.neg = sum(!true.class),
+                 tpr.fpr = tpr.fpr,
+                 tpr.fpr.raw = tpr.fpr.raw,
+                 time.used = bench,
+                 auc = auc,
+                 tpr.fpr.boot.matrix = tpr.fpr.boot)  
+  class(output) <- append(class(output), "fbroc.roc")
+  return(output)
+}
+
+
+#' Generates confidence intervals for the TPR for a range of FPRs
+#' 
+#' Calculates confidence intervals for the TPR at different FPR values. 
+#' This function is also used to plot the confidence regions in the
+#' function \code{\link{plot.fbroc.roc}}.
+#' 
+#' @param roc Object of class \code{fbroc.roc}.
+#' @param conf.level Confidence level to be used for the confidence intervals.
+#' @param steps Number of discrete steps for the FPR at which the TPR is 
+#' calculated. TPR confidence intervals are given for all FPRs in 
+#' \code{seq(0, 1, by = (1 / steps))}.
+#' @return A data.frame containing the FPR steps and the lower and upper bounds
+#' of the confidence interval for the TPR.
+#' @export
+#' @seealso \code{\link{boot.roc}}
+conf.roc <- function(roc, conf.level = 0.95, steps = 100) {
+  alpha <- 0.5*(1 - conf.level)
+  alpha.levels <- c(alpha, 1 - alpha) 
+  steps = as.integer(steps)
+  # translate tpr_fpr at threshold matrix into tpr at fpr matrix
+  rel.matrix <- get_tpr_matrix(roc$tpr.fpr.boot.matrix, steps)
+  rm(roc)
+  conf.area <- t(apply(rel.matrix, 2, quantile, alpha.levels))
+  conf.area <- as.data.frame(conf.area)
+  names(conf.area) <- c("Lower.TPR", "Upper.TPR")
+  conf.area <- cbind(data.frame(FPR = 1 - seq(0, 1, by = (1 / steps))), conf.area)
+  return(conf.area)
+}
+
+#' Process bootstrapped TPR/FPR at thresholds matrix into TPR at FPR matrix
+#' 
+#' Function \code{boot.roc} contains the TPR and FPR result at each threshold
+#' per bootstrap replicate. This is easy to calculate, but often not convenient
+#' to work with. Therefore \code{boot.tpr.at.fpr} transform that matrix
+#' so that in each column are the bootstrap results for the TPR at a specific
+#' FPR.
+#' @param roc Object of class \code{fbroc.roc}.
+#' @param steps Number of discrete steps for the FPR at which the TPR is 
+#' calculated. TPR confidence intervals are given for all FPRs in 
+#' \code{seq(0, 1, by = (1 / steps))}.
+#' @return Matrix containing the TPR bootstrap replicates for the discrete
+#' FPR steps.
+#' @export
+#' @seealso \code{\link{boot.roc}}
+boot.tpr.at.fpr <- function(roc, steps) {
+  steps = as.integer(steps)
+  rel.matrix <- get_tpr_matrix(roc$tpr.fpr.boot.matrix, steps)
+  FPR.VEC = round(1 - seq(0, 1, by = (1 / steps),3))
+  colnames(rel.matrix) <- paste("TPR.AT.FPR.", FPR.VEC, sep = "")
+  return(rel.matrix)
+}
+
+#' Calculates ROC curve thresholds
+#' 
+#' \code{calculate.thresholds} calculates the thresholds of the ROC curve
+#' at which the curve changes directions. 
+#' 
+#' @param pred A numeric vector. Contains predictions. \code{calculate.thresholds} 
+#'   assumes that a high prediction is evidence for the observation belonging 
+#'   to the positive class.
+#' @param true.class A logical vector. TRUE indicates the sample belonging to the
+#'   positive class.
+#' @return A numeric vector containing the thresholds. The length of the vector
+#'   depends on the data. The number of thresholds tends to go down as the
+#'   performance of the clasifier improves.   
+#' 
+#' @examples
+#' x <- 1:10
+#' y <- c(FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
+#' calculate.thresholds(x, y) # relevant thresholds are 1, 4, 5, 7, 11
+#' 
+#' @export
+calculate.thresholds <- function(pred, true.class) {
+  pred <- c(pred, max(pred) + 1) # add one threshold since we use >
+  index <- order(pred)
+  pred <- pred[index]
+  true.class <- true.class[index]
+  is.threshold <- find_thresholds(pred, true.class) # use C++ to find thresholds
+  thresholds <- pred[as.logical(is.threshold)]
+  return(thresholds)
+}
